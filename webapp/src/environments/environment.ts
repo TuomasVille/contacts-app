b27532f89bd6c1421bad89f77ad97f9e3c38f058
TuomasVille <tuomas_kayhty@hotmail.com>
2019-04-08 11:35:16 +0300

    Git commit

--- webapp/src/environments/environment.ts
+++ webapp/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiEndpointUrl: 'https://contactswebapi.azurewebsites.net/',
+  apiEndpointUrl: 'http://localhost:3000',
   apiEnabled: false
 };
 
