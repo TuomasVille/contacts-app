cac6e2d19b1ecb767ff8c067fe6daf4a897161da
TuomasVille <tuomas_kayhty@hotmail.com>
2019-04-08 10:41:01 +0300

    Git commit

--- webapp/src/app/contact/service/contact-http.service.ts
+++ webapp/src/app/contact/service/contact-http.service.ts
@@ -14,7 +14,7 @@ export class ContactHttpService implements ContactProvider {
   url: string;
 
   constructor(private httpClient: HttpClient) {
-    this.url = environment.apiEndpointUrl + '/api/contacts';
+    this.url = environment.apiEndpointUrl + '/contacts';
   }
 
   search(name: string): Observable<Contact[]> {
